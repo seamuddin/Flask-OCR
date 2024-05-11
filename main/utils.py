a285cf403b6651f9467cdc824b1779d9fe16731e
seamuddin <seamuddin2020@gmail.com>
2024-05-11 14:16:48 +0600

    New Modification

--- main/utils.py
+++ main/utils.py
@@ -1,13 +1,14 @@
 from pdf2image import convert_from_path
 import pytesseract
 
+
 def convert_pdf_to_text(image_path):
-    images = convert_from_path(image_path, 250)
+    pop_path = '/usr/bin/pdfinfo'
+    images = convert_from_path(image_path, 500, poppler_path=pop_path)
 
     extracted_text = ''
     for page in images:
         text = pytesseract.image_to_string(page, lang='eng+ben', config='--psm 6')
         extracted_text += text + '\n'  # Add a newline between pages if needed
-    
-    return extracted_text
 
+    return extracted_text
