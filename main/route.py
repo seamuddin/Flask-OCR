102988137cf1103e9e53b5bba668fe952768b89c
seamuddin <seamuddin2020@gmail.com>
2024-05-11 22:29:26 +0600

    New Modification

--- main/route.py
+++ main/route.py
@@ -16,25 +16,20 @@ def get_token_route():
 @route.route("/upload", methods=["POST","GET"])
 def uplaod_pdf():
     if request.method == 'POST':
-        # Check if a file is included in the request
         if 'pdf' not in request.files:
             return jsonify({'error': 'No PDF file provided'}), 400
 
         pdf_file = request.files['pdf']
 
-        # Check if the file has an allowed extension
         if not allowed_file(pdf_file.filename):
             return jsonify({'error': 'Unsupported file type'}), 400
 
-        # Create the uploads folder if it doesn't exist
         if not os.path.exists(UPLOAD_FOLDER):
             os.makedirs(UPLOAD_FOLDER)
 
-        # Save the uploaded file to the uploads folder
         pdf_path = os.path.join(UPLOAD_FOLDER, pdf_file.filename)
         pdf_file.save(pdf_path)
 
-        # Convert the PDF to text
         text = convert_pdf_to_text(pdf_path)
 
         return jsonify({'data': text}), 200
